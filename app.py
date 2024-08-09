1bd3042c5277e13c141824f7e4dd542fcdf405fb
Sharafunneesa-pp <127863253+Sharafunneesa-pp@users.noreply.github.com>
2024-08-09 13:05:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import ChatOpenAI
 # from langchain_chroma import Chroma
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 
 from langchain.chains import ConversationalRetrievalChain
 from dotenv import load_dotenv
