4c6733fc4e21e52b107808ec16347f73d1155a8d
Sharafunneesa-pp <127863253+Sharafunneesa-pp@users.noreply.github.com>
2024-08-09 12:32:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import ChatOpenAI
